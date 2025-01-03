acce7eeecbf16b7c6525cbe4057aec2f6a2be6a0
Idir.D <idirdehmous@gmail.com>
2025-01-03 14:17:23 +0100

    C code

--- pycc/fetch_module.c
+++
@@ -1,1 +0,0 @@
-src/fetch_module.c
--- pycc/src/fetch_module.c
+++ pycc/src/fetch_module.c
@@ -98,7 +98,7 @@ if ( verbose  )   {
   size_t prog_max = 0  ;
 
  
-  if ( lpbar  ) {
+  if ( lpbar  == 1 ) {
    int  total_rows =getMaxrows(database, sql_query , fcols   )  ; 
    prog_max = (size_t) total_rows ; 
   }
@@ -143,7 +143,7 @@ if ( verbose  )   {
 
     while ( (nd = nextrow(h, d, dlen, &new_dataset)) > 0) {	    
 
-      if ( pbar  ) {
+      if ( lpbar  == 1 ) {
       ip=ip+1 ; 
       print_progress(ip  , prog_max  );
       }

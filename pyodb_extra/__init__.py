97f16625bfe2b70864e62e4306cce0cc5f6c5a62
idir Dehmous <idirdehmous@gmail.com>
2025-11-14 18:50:56 +0100

    Add import in __init__

--- pyodb_extra/__init__.py
+++ pyodb_extra/__init__.py
@@ -3,6 +3,14 @@ import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
+
+def main():
+    """Entry point for the application script"""
+    print("Call your main application code here")
+
+
+
 __version__ = "_1.1.0"
 
 from  .parser        import *
@@ -11,6 +19,5 @@ from  .odb_glossary  import *
 from  .exceptions    import *
 from  .environment   import *
 from  .dr_hook       import * 
-from  .pool_factory  import * 
 
 

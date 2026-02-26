45202681170cf8ad9bd0f5a998466234d0cb81ed
rmibdev <rmib.dev@gmail.com>
2026-02-26 15:24:03 +0000

    reset classes to pyodb

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,3 +1,7 @@
+# pyodb
+# Copyright (C) 2026 Royal Meteorological Institute of Belgium (RMI)
+#
+# Licensed under the Apache License, Version 2.0
 # -*- coding: utf-8 -*-
 
 __all__ =[  "pyodbErrMessage"        ,
